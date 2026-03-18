3f19b3ca8f567caafc90604f4e31992d57a24617
bandu5878-cloud <bandu5878@gmail.com>
2026-03-19 02:46:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+from EmotionDetection import emotion_detector
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text = request.args.get('textToAnalyze')
+
+    if not text:
+        return "Invalid input! Please try again."
+
+    result = emotion_detector(text)
+
+    if "error" in result:
+        return "Invalid input! Please try again."
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
